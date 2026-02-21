538b427b9b55134365bd684fc97d7b54144458f7
adazeynep <adazeynepozkan@posta.mu.edu.tr>
2026-02-21 14:30:07 +0300

    week 1 tasks

---
+++ week1/week1.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MerhabaDunyaUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ekrana yazıyı yazdıran komut
+            Console.WriteLine("Hello, World!");
+
+            // Konsol penceresinin hemen kapanmaması için bir tuşa basılmasını bekler
+            Console.ReadKey();
+        }
+    }
+}
